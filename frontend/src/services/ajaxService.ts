7833a14f2b8c9b630518a4efa6ae19b5eea16476
lainem25 <mikael.2.laine@aalto.fi>
2024-04-12 10:46:46 +0300

    Update env variables

--- frontend/src/services/ajaxService.ts
+++ frontend/src/services/ajaxService.ts
@@ -1,20 +1,16 @@
 import { Dayjs } from "dayjs"
 
 export const getTodayFloridaMan = async () => {
-  const response = await fetch(
-    `${import.meta.env.VITE_APIROOT}/floridaman/today`,
-    { method: "POST" }
-  )
+  const response = await fetch(`${import.meta.env.APIROOT}/floridaman/today`, {
+    method: "POST",
+  })
   return await response.json()
 }
 
 export const getDateFloridaMan = async (date: Dayjs) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_APIROOT}/floridaman/date`,
-    {
-      method: "POST",
-      body: JSON.stringify({ day: date.date(), month: date.month() }),
-    }
-  )
+  const response = await fetch(`${import.meta.env.APIROOT}/floridaman/date`, {
+    method: "POST",
+    body: JSON.stringify({ day: date.date(), month: date.month() }),
+  })
   return await response.json()
 }
